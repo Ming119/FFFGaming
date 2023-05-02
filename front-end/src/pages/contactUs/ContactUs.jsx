ddc6fe765ea36dcca3a87146ee1f9d2704d3b00f
Ming119 <ming2007119@gmail.com>
2023-05-02 20:33:56 +0800

    finish contactus page

--- front-end/src/pages/contactUs/ContactUs.jsx
+++ front-end/src/pages/contactUs/ContactUs.jsx
@@ -12,23 +12,24 @@ const ContactUs = () => {
 
       <Row className="my-3 gx-5">
         <Col sm={ 12 } md={ 6 } className="border-end p-5">
-          <div className="border border-2 rounded rounded-5" style={{ paddingTop: "50%", paddingBottom: "50%" }}>
-            <Button variant="link" className="w-100">
-              <Envelope style={{ width: '2em', height: '2em' }} />
+          <div className="border border-2 rounded rounded-5">
+            <Button variant="link" className="w-100" style={{ paddingTop: "40%", paddingBottom: "40%" }}>
+              <Envelope style={{ width: '2em', height: '2em' }} /><br />
+              Email
             </Button>
           </div>
         </Col>
 
         <Col sm={ 12 } md={ 6 } className="border-start p-5">
-          <div className="border border-2 rounded rounded-5" style={{ paddingTop: "50%", paddingBottom: "50%" }}>
+          <div className="border border-2 rounded rounded-5">
               <ButtonGroup className="w-100">
-                <Button variant="link" className="w-100">
-                  <Discord style={{ width: '2em', height: '2em' }} />
-                  Discord
+                <Button variant="link" className="w-100" style={{ paddingTop: "40%", paddingBottom: "40%" }}>
+                  <Discord style={{ width: '2em', height: '2em' }} /> <br />
+                  Discord 私訊
                 </Button>
-                <Button variant="link" className="w-100">
-                  <Discord style={{ width: '2em', height: '2em' }} />
-                  Discord
+                <Button variant="link" className="w-100" style={{ paddingTop: "40%", paddingBottom: "40%" }}>
+                  <Discord style={{ width: '2em', height: '2em' }} /><br />
+                  加入 Discord 群組
                 </Button>
               </ButtonGroup>
           </div>
