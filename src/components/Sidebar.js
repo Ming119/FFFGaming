7ea1f711808542a616f3f22a7a21677d9c6634b2
Ming119 <ming2007119@gmail.com>
2023-07-21 23:27:33 +0800

    fix merge

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,77 +1,89 @@
-import React, { useState } from 'react';
-import { Col, Nav, Row, Tab, Collapse } from 'react-bootstrap';
-import AllProducts from '../pages/sidebar/AllProducts';
-import NewProducts from '../pages/sidebar/NewProducts';
-import PopularProducts from '../pages/sidebar/PopularProducts';
+import React, {useState} from 'react';
+import { Col, Nav, Row, Tab } from 'react-bootstrap';
+import '../assets/css/sidebar.css';
+import Products from '../pages/sidebar/Products';
+import { useLoaderData } from 'react-router-dom';
 
 export const Sidebar = (props) => {
-  const [open, setOpen] = useState(false);
 
-    return (
-      <Tab.Container id="left-tabs-example" defaultActiveKey="AllProducts">
+  const { categories, products } = useLoaderData();
+  const [activeTab, setActiveTab] = useState('AllProducts');
+
+  const handleTabChange = (eventKey) => {
+    setActiveTab(eventKey);
+  };
+
+  return (
+      <Tab.Container id="left-tabs-example" activeKey={activeTab} onSelect={handleTabChange}>
         <Row>
           <Col sm={3}>
-            <Nav variant="pills" className="flex-column">
-              <h3>Product Categories</h3>
-              <Nav.Item>
-                <Nav.Link eventKey="AllProducts">All Products</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="NewProducts">New Products</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="PopularProducts">Popular Products</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
+            <Nav className="list-unstyled ps-0 p-3 flex-column">
+              <Nav.Item className="mb-1">
                 <Nav.Link
-                  onClick={() => setOpen(!open)}
-                  aria-controls="example-collapse-text"
-                  aria-expanded={open}          
+                  as="button"
+                  className="btn btn-no-toggle align-items-center rounded"
+                  eventKey="AllProducts"
                 >
-                  {open ? <i class="bi bi-chevron-down"></i> : <i class="bi bi-chevron-right"></i>}
-                  <span>&nbsp;3C Products</span>
+                  全部產品
                 </Nav.Link>
-                <Collapse in={open}>
-                  <div>
-                    <Nav.Item>
-                      <Nav.Link eventKey="ProductTest">Product Test</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="test1">test 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="test2">test 2</Nav.Link>
-                    </Nav.Item>
+              </Nav.Item>
+              { categories.map((category) => (
+                <Nav.Item className="mb-1" key={category.id}>
+                  <Nav.Link
+                    as="button"
+                    className="btn btn-toggle align-items-center rounded collapsed"
+                    data-bs-toggle="collapse"
+                    data-bs-target={`#collapse${category.id}`}
+                    aria-expanded="true"
+                  >
+                    {category.name}
+                  </Nav.Link>
+                  <div className="collapse show" id={`collapse${category.id}`}>
+                    <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                      { category.sub.map((subcategory) => (
+                        <li key={`${category.id}_${subcategory.id}`}>
+                          <Nav.Link className="link-dark rounded" eventKey={`${category.id}_${subcategory.id}`}>{subcategory.name}</Nav.Link>
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                </Collapse>
+                </Nav.Item>
+              )) }
+              <Nav.Item className="mb-1">
+                <Nav.Link
+                  as="button"
+                  className="btn btn-no-toggle align-items-center rounded"
+                  aria-expanded="false"
+                  eventKey="DIY"
+                >
+                  客製化專區
+                </Nav.Link>
               </Nav.Item>
             </Nav>
           </Col>
+
           <Col sm={9}>
             <Tab.Content>
               <Tab.Pane eventKey="AllProducts">
-                <AllProducts />
+                <Products title={"全部產品"} products={ products }/>
               </Tab.Pane>
-              <Tab.Pane eventKey="NewProducts">
-                <NewProducts />
-              </Tab.Pane>
-              <Tab.Pane eventKey="PopularProducts">
-                <PopularProducts />
-              </Tab.Pane>
-              <Tab.Pane eventKey="ProductTest">
-                <h3>Product Test</h3>
-              </Tab.Pane>
-              <Tab.Pane eventKey="test1">
-                <h3>test1</h3>
-              </Tab.Pane>
-              <Tab.Pane eventKey="test2">
-                <h3>test2</h3>
+              { categories.map((category) => (
+                category.sub.map((subcategory) => (
+                  <Tab.Pane eventKey={`${category.id}_${subcategory.id}`} key={`${category.id}_${subcategory.id}`}>
+                    <Products
+                      title={`${category.name} - ${subcategory.name}`}
+                      products={ products.filter(product => product.category === `${category.id}_${subcategory.id}`) }/>
+                  </Tab.Pane>
+                ))
+              )) }
+              <Tab.Pane eventKey="DIY">
+                <Products title={"客製化專區"} />
               </Tab.Pane>
             </Tab.Content>
           </Col>
         </Row>
       </Tab.Container>
-    );
+  );
 };
 
 export default Sidebar;
